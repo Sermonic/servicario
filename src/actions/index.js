394663b5605dc2f5d8d6b0c59ebcc893e0f08539
Sermonic <vitaliyshcherbanych@gmail.com>
2020-07-03 13:15:45 +0300

    Add API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,32 +1,17 @@
 import { FETCH_SERVICES_SUCCESS, FETCH_SERVICE_SUCCESS } from '../types'
 
-import db from '../db'
+import * as api from '../api'
 
 export const fetchServices = () => {
-  return db
-    .collection('services')
-    .get()
-    .then((snapshot) => {
-      const services = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }))
-      return {
-        type: FETCH_SERVICES_SUCCESS,
-        services,
-      }
-    })
+  return api.fetchServices().then((services) => ({
+    type: FETCH_SERVICES_SUCCESS,
+    services,
+  }))
 }
 
 export const fetchServiceById = (serviceId) => {
-  return db
-    .collection('services')
-    .doc(serviceId)
-    .get()
-    .then((snapshot) => {
-      return {
-        type: FETCH_SERVICE_SUCCESS,
-        service: { id: snapshot.id, ...snapshot.data() },
-      }
-    })
+  return api.fetchServiceById(serviceId).then((service) => ({
+    type: FETCH_SERVICE_SUCCESS,
+    service,
+  }))
 }
