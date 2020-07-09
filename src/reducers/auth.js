41516ab3c633b73a119c2ea3c94a09a7b680cd79
Sermonic <vitaliyshcherbanych@gmail.com>
2020-07-09 19:41:05 +0300

    Reset auth state

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,4 +1,4 @@
-import { SET_AUTH_USER } from '../types'
+import { SET_AUTH_USER, RESET_AUTH_STATE } from '../types'
 
 const INITIAL_STATE = {
   user: null,
@@ -10,6 +10,8 @@ const auth = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case SET_AUTH_USER:
       return { user: action.user, isAuth: !!action.user, isAuthResolved: true }
+    case RESET_AUTH_STATE:
+      return { ...state, isAuthResolved: false }
     default:
       return state
   }
