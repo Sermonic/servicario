aa006e0c2bf25cfbbf42f2fff63b43f0c7692778
Sermonic <vitaliyshcherbanych@gmail.com>
2020-07-15 15:06:35 +0300

    Display Offers & Extract Data

--- src/actions/offers.js
+++ src/actions/offers.js
@@ -3,16 +3,44 @@ import * as api from '../api'
 
 export const createOffer = (offer) => api.createOffer(offer)
 
+const extractDataFromOffer = async (offer, userType) => {
+  const service = await offer.service.get()
+  const user = await offer[userType].get()
+
+  offer.service = service.data()
+  offer[userType] = user.data()
+
+  return offer
+}
+
 export const fetchSentOffers = (userId) => (dispatch) => {
-  return api.fetchSentOffers(userId).then((offers) => {
-    dispatch({ type: FETCH_OFFERS_SUCCESS, offers, offersType: 'sent' })
+  return api.fetchSentOffers(userId).then(async (offers) => {
+    const mappedOffers = await Promise.all(
+      offers.map((offer) => extractDataFromOffer(offer, 'toUser'))
+    )
+
+    dispatch({
+      type: FETCH_OFFERS_SUCCESS,
+      offers: mappedOffers,
+      offersType: 'sent',
+    })
+
     return offers
   })
 }
 
 export const fetchReceivedOffers = (userId) => (dispatch) => {
-  return api.fetchReceivedOffers(userId).then((offers) => {
-    dispatch({ type: FETCH_OFFERS_SUCCESS, offers, offersType: 'received' })
+  return api.fetchReceivedOffers(userId).then(async (offers) => {
+    const mappedOffers = await Promise.all(
+      offers.map((offer) => extractDataFromOffer(offer, 'fromUser'))
+    )
+
+    dispatch({
+      type: FETCH_OFFERS_SUCCESS,
+      offers: mappedOffers,
+      offersType: 'received',
+    })
+
     return offers
   })
 }
--- src/pages/offers/SentOffers.js
+++ src/pages/offers/SentOffers.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { fetchSentOffers } from '../../actions'
 import withAuthorization from '../../component/hoc/withAuthorization'
 import ServiceItem from '../../component/service/ServiceItem'
@@ -11,37 +12,40 @@ class SentOffers extends React.Component {
   }
 
   render() {
+    const { offers } = this.props
+
     return (
       <div className='container'>
         <div className='content-wrapper'>
           <h1 className='title'>Sent Offers</h1>
           <div className='columns'>
-            <div className='column is-one-third'>
-              {/* <ServiceItem
-              noButton
-              className="offer-card"
-              service={o.service}>
-              <div className="tag is-large">
-                {o.status}
+            {offers.map((offer) => (
+              <div key={offer.id} className='column is-one-third'>
+                <ServiceItem
+                  noButton
+                  className='offer-card'
+                  service={offer.service}
+                >
+                  <div className='tag is-large'>{offer.status}</div>
+                  <hr />
+                  <div className='service-offer'>
+                    <div>
+                      <span className='label'>From User:</span>{' '}
+                      {offer.fromUser.fullName}
+                    </div>
+                    <div>
+                      <span className='label'>Note:</span> {offer.note}
+                    </div>
+                    <div>
+                      <span className='label'>Price:</span> ${offer.price}
+                    </div>
+                    <div>
+                      <span className='label'>Time:</span> {offer.time} hours
+                    </div>
+                  </div>
+                </ServiceItem>
               </div>
-              <hr />
-              <div className="service-offer">
-                <div>
-                  <span className="label">From User:</span> {o.toUser.fullName}
-                </div>
-                <div>
-                  <span className="label">Note:</span> {o.note}
-                </div>
-                <div>
-                  <span className="label">Price:</span> ${o.price}
-                </div>
-                <div>
-                  <span className="label">Time:</span> {o.time} hours
-                </div>
-              </div>
-            </ServiceItem>
-            */}
-            </div>
+            ))}
           </div>
         </div>
       </div>
@@ -49,4 +53,6 @@ class SentOffers extends React.Component {
   }
 }
 
-export default withAuthorization(SentOffers)
+const mapStateToProps = ({ offers }) => ({ offers: offers.sent })
+
+export default withAuthorization(connect(mapStateToProps)(SentOffers))
